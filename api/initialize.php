ad539df51cd200fbc27cf0cb30425102164fd750
Diego Garza <dgarza8988@gmail.com>
2019-08-04 06:07:42 -0400

    Key and source come in from request

--- api/initialize.php
+++ api/initialize.php
@@ -1,4 +1,20 @@
 <?php
+function getUserIp()
+{
+    if (array_key_exists('HTTP_CLIENT_IP', $_SERVER) && !empty($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') > 0) {
+            $addr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim($addr[0]);
+        } else {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+    return null;
+}
   // required headers
   header("Access-Control-Allow-Origin: *");
   header("Content-Type: application/json; charset=UTF-8");
@@ -6,16 +22,17 @@
   header("Access-Control-Max-Age: 3600");
   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-  include_once '../usaepay.php';
+  include_once dirname(__FILE__).'/../usaepay.php';
+
   $tran=new umTransaction;
   $data = json_decode(file_get_contents("php://input"));
 
-  $tran->key="sW4rZ250lRzRz5PpOlU345M8210rr1G0"; // source key
-  $tran->pin="98761234"; // source key pin
+  $tran->key=$data->key; // source key
+  $tran->pin=$data->pin; // source key pin
   $tran->usesandbox=false; // Sandbox true/false
 
-  // $tran->ip=$REMOTE_ADDR; // This allows fraud blocking on the customers ip address 
-  $tran->testmode=1;
+  $tran->ip=getUserIp(); // This allows fraud blocking on the customers ip address 
+  $tran->testmode=$data->test_mode;
 
   $token;
 ?>
